51e2604292958ae9d208797e3e93a486c5c8ea5a
Omar1123 <omar1123veliz@gmail.com>
2017-06-06 19:13:27 -0600

    ADD: C examples

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: CPAREDES
+ *
+ * Created on 7 de febrero de 2017, 07:59 PM
+ */
+
+#include <cstdlib>
+#include <stdio>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hola mundo"<<endl;
+    return 0;
+}
+
